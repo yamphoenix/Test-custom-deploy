3e6120206f5c449103f2837dd2eb141db1111c60
yashita.mittal <yashita.mittal@a8c.com>
2026-03-27 16:09:12 +0530

    Add GitHub Actions workflow and Hello Dolly test plugin

---
+++ plugins/hello-dolly.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Hello Dolly
+ * Plugin URI: https://wordpress.org/plugins/hello-dolly/
+ * Description: This is a test plugin for custom deployment.
+ * Version: 1.0
+ * Author: Yashita
+ */
+
+function hello_dolly() {
+    $lyrics = "Hello, Dolly, well, hello, Dolly!";
+    echo "<p style='float:right;padding:10px;'>$lyrics</p>";
+}
+
+add_action( 'admin_notices', 'hello_dolly' );
